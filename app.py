51bea80d4d29492b34d7555f7e2e492aedb2e2e9
Jose Miguel Aguilar Torrealba <66124263+Migue1804@users.noreply.github.com>
2024-02-25 13:52:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,86 +2,53 @@ import streamlit as st
 import matplotlib.pyplot as plt
 
 def evaluate_okr(objective, key_results):
-    okr_pass = True
-    feedback = []
+    okr_pass = []
 
     # Evaluar preguntas sobre el objetivo
     objective_clear = st.radio("¿El objetivo está claramente definido y alineado con la visión estratégica de la organización?", ("Sí", "No"))
-    objective_clear_comment = st.text_area("Comentarios adicionales (opcional):", key="objective_clear_comment")
-    if objective_clear == "No":
-        okr_pass = False
-        feedback.append(f"El objetivo no está claramente definido y alineado con la visión estratégica de la organización. {objective_clear_comment}")
+    okr_pass.append(objective_clear == "Sí")
 
     objective_ambitious = st.radio("¿El objetivo es ambicioso pero alcanzable?", ("Sí", "No"))
-    objective_ambitious_comment = st.text_area("Comentarios adicionales (opcional):", key="objective_ambitious_comment")
-    if objective_ambitious == "No":
-        okr_pass = False
-        feedback.append(f"El objetivo no es ambicioso pero alcanzable. {objective_ambitious_comment}")
+    okr_pass.append(objective_ambitious == "Sí")
 
     objective_relevant = st.radio("¿El objetivo es relevante y significativo para el éxito de la organización?", ("Sí", "No"))
-    objective_relevant_comment = st.text_area("Comentarios adicionales (opcional):", key="objective_relevant_comment")
-    if objective_relevant == "No":
-        okr_pass = False
-        feedback.append(f"El objetivo no es relevante y significativo para el éxito de la organización. {objective_relevant_comment}")
+    okr_pass.append(objective_relevant == "Sí")
 
     objective_understandable = st.radio("¿El objetivo es comprensible y motivador para los equipos?", ("Sí", "No"))
-    objective_understandable_comment = st.text_area("Comentarios adicionales (opcional):", key="objective_understandable_comment")
-    if objective_understandable == "No":
-        okr_pass = False
-        feedback.append(f"El objetivo no es comprensible y motivador para los equipos. {objective_understandable_comment}")
+    okr_pass.append(objective_understandable == "Sí")
 
     # Evaluar preguntas sobre los resultados clave
     key_results_specific = st.radio("¿Los resultados clave son específicos y medibles?", ("Sí", "No"))
-    key_results_specific_comment = st.text_area("Comentarios adicionales (opcional):", key="key_results_specific_comment")
-    if key_results_specific == "No":
-        okr_pass = False
-        feedback.append(f"Los resultados clave no son específicos y medibles. {key_results_specific_comment}")
+    okr_pass.append(key_results_specific == "Sí")
 
     key_results_clear_progress = st.radio("¿Los resultados clave proporcionan una indicación clara de progreso hacia el logro del objetivo?", ("Sí", "No"))
-    key_results_clear_progress_comment = st.text_area("Comentarios adicionales (opcional):", key="key_results_clear_progress_comment")
-    if key_results_clear_progress == "No":
-        okr_pass = False
-        feedback.append(f"Los resultados clave no proporcionan una indicación clara de progreso hacia el logro del objetivo. {key_results_clear_progress_comment}")
+    okr_pass.append(key_results_clear_progress == "Sí")
 
     key_results_realistic = st.radio("¿Los resultados clave son realistas y factibles dentro del marco de tiempo establecido?", ("Sí", "No"))
-    key_results_realistic_comment = st.text_area("Comentarios adicionales (opcional):", key="key_results_realistic_comment")
-    if key_results_realistic == "No":
-        okr_pass = False
-        feedback.append(f"Los resultados clave no son realistas y factibles dentro del marco de tiempo establecido. {key_results_realistic_comment}")
+    okr_pass.append(key_results_realistic == "Sí")
 
     key_results_relevant = st.radio("¿Los resultados clave son relevantes para el objetivo y contribuyen significativamente a su logro?", ("Sí", "No"))
-    key_results_relevant_comment = st.text_area("Comentarios adicionales (opcional):", key="key_results_relevant_comment")
-    if key_results_relevant == "No":
-        okr_pass = False
-        feedback.append(f"Los resultados clave no son relevantes para el objetivo y contribuyen significativamente a su logro. {key_results_relevant_comment}")
+    okr_pass.append(key_results_relevant == "Sí")
 
     # Evaluar preguntas sobre la cascada de OKRs
     cascading_okrs = st.radio("¿El OKR está desglosado en OKRs específicos y medibles para cada equipo o departamento?", ("Sí", "No"))
-    cascading_okrs_comment = st.text_area("Comentarios adicionales (opcional):", key="cascading_okrs_comment")
-    if cascading_okrs == "No":
-        okr_pass = False
-        feedback.append(f"El OKR no está desglosado en OKRs específicos y medibles para cada equipo o departamento. {cascading_okrs_comment}")
+    okr_pass.append(cascading_okrs == "Sí")
 
     okrs_aligned = st.radio("¿Los OKRs de los equipos están alineados con los objetivos estratégicos de nivel superior?", ("Sí", "No"))
-    okrs_aligned_comment = st.text_area("Comentarios adicionales (opcional):", key="okrs_aligned_comment")
-    if okrs_aligned == "No":
-        okr_pass = False
-        feedback.append(f"Los OKRs de los equipos no están alineados con los objetivos estratégicos de nivel superior. {okrs_aligned_comment}")
+    okr_pass.append(okrs_aligned == "Sí")
 
     okrs_consistent = st.radio("¿Existe coherencia y consistencia en la cascada de OKRs a través de la organización?", ("Sí", "No"))
-    okrs_consistent_comment = st.text_area("Comentarios adicionales (opcional):", key="okrs_consistent_comment")
-    if okrs_consistent == "No":
-        okr_pass = False
-        feedback.append(f"No existe coherencia y consistencia en la cascada de OKRs a través de la organización. {okrs_consistent_comment}")
+    okr_pass.append(okrs_consistent == "Sí")
 
     # Concluir evaluación del OKR
     st.write("Resultado de la evaluación del OKR:")
-    if okr_pass:
+    if all(okr_pass):
         st.write("El OKR está bien definido.")
     else:
         st.write("El OKR no está bien definido. Motivos:")
-        for reason in feedback:
-            st.write(f"- {reason}")
+        for idx, question in enumerate(okr_questions):
+            if not okr_pass[idx]:
+                st.write(f"- {question}")
 
     # Mostrar el gráfico
     plot_results(okr_pass)
@@ -105,8 +72,6 @@ def plot_results(okr_pass):
     st.pyplot(fig)
 
 def main():
-    # Agregar una imagen en la parte superior
-    st.image("app.jpg", width=700)  # Reemplaza "app.jpg" con la ruta de tu imagen y ajusta el ancho según sea necesario
     st.title("App de Evaluación de OKRs")
     # Interfaz para ingresar el objetivo y los resultados clave
     objective = st.text_input("Objetivo:")
@@ -115,5 +80,21 @@ def main():
     # Evaluar el OKR
     evaluate_okr(objective, key_results)
 
+# Preguntas para la evaluación del OKR
+okr_questions = [
+    "¿El objetivo está claramente definido y alineado con la visión estratégica de la organización?",
+    "¿El objetivo es ambicioso pero alcanzable?",
+    "¿El objetivo es relevante y significativo para el éxito de la organización?",
+    "¿El objetivo es comprensible y motivador para los equipos?",
+    "¿Los resultados clave son específicos y medibles?",
+    "¿Los resultados clave proporcionan una indicación clara de progreso hacia el logro del objetivo?",
+    "¿Los resultados clave son realistas y factibles dentro del marco de tiempo establecido?",
+    "¿Los resultados clave son relevantes para el objetivo y contribuyen significativamente a su logro?",
+    "¿El OKR está desglosado en OKRs específicos y medibles para cada equipo o departamento?",
+    "¿Los OKRs de los equipos están alineados con los objetivos estratégicos de nivel superior?",
+    "¿Existe coherencia y consistencia en la cascada de OKRs a través de la organización?"
+]
+
 if __name__ == "__main__":
     main()
+
