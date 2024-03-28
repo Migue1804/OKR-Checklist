1ae8ea41a2c554da4ffe7e52bc33905d245e7460
Jose Miguel Aguilar Torrealba <66124263+Migue1804@users.noreply.github.com>
2024-03-28 12:55:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -104,8 +104,38 @@ def evaluate_okr(objective, key_results, okr_questions):
 
     # Verificar si hay comentarios adicionales ingresados
     if len(comentarios_adicionales) > 0:
-        # Create and generate a word cloud image
-        wordcloud = WordCloud().generate(comentarios_adicionales)
+        # Crear y generar la imagen del WordCloud
+        stopwords = [
+            'a', 'al', 'algo', 'algunas', 'algunos', 'ante', 'antes', 'como', 'con', 'contra', 'cual', 'cuales',
+            'cuando', 'de', 'del', 'desde', 'donde', 'durante', 'e', 'el', 'ella', 'ellas', 'ellos', 'en', 'entre',
+            'era', 'erais', 'eramos', 'eran', 'eras', 'eres', 'es', 'esa', 'esas', 'ese', 'eso', 'esos', 'esta', 'estaba',
+            'estabais', 'estaban', 'estabas', 'estamos', 'estan', 'estando', 'estar', 'estaremos', 'estara', 'estarán',
+            'estarás', 'estaré', 'estaréis', 'estaria', 'estaríais', 'estaríamos', 'estarían', 'estarías', 'este',
+            'estemos', 'esto', 'estos', 'estoy', 'estuve', 'estuviera', 'estuvierais', 'estuvieran', 'estuvieras',
+            'estuvieron', 'estuviese', 'estuvieseis', 'estuviesen', 'estuvieses', 'estuvimos', 'estuviste', 'estuvisteis',
+            'estuvieramos', 'estuviesemos', 'estuvo', 'está', 'estábamos', 'estáis', 'están', 'estás', 'esté', 'estéis',
+            'estén', 'estés', 'fue', 'fuera', 'fuerais', 'fueran', 'fueras', 'fueron', 'fuese', 'fueseis', 'fuesen',
+            'fueses', 'fui', 'fuimos', 'fuiste', 'fuisteis', 'fuéramos', 'fuésemos', 'ha', 'habida', 'habidas', 'habido',
+            'habidos', 'habiendo', 'habremos', 'habrá', 'habrán', 'habrás', 'habré', 'habréis', 'habria', 'habría',
+            'habriais', 'habríais', 'habriamos', 'habríamos', 'habrian', 'habrían', 'habrias', 'habrías', 'habéis', 'había',
+            'habíais', 'habíamos', 'habían', 'habías', 'han', 'has', 'hasta', 'hay', 'haya', 'hayamos', 'hayan', 'hayas',
+            'hayáis', 'he', 'hemos', 'hubiera', 'hubierais', 'hubieran', 'hubieras', 'hubieron', 'hubiese', 'hubieseis',
+            'hubiesen', 'hubieses', 'hubimos', 'hubiste', 'hubisteis', 'hubiéramos', 'hubiésemos', 'hubo', 'la', 'las',
+            'le', 'les', 'lo', 'los', 'me', 'mi', 'mis', 'mucho', 'muchos', 'muy', 'más', 'nos', 'nosotras', 'nosotros',
+            'nuestra', 'nuestras', 'nuestro', 'nuestros', 'o', 'os', 'otra', 'otras', 'otro', 'otros', 'para', 'pero',
+            'poco', 'por', 'porque', 'que', 'quien', 'quienes', 'qué', 'se', 'sea', 'seamos', 'sean', 'seas', 'sentido',
+            'ser', 'seremos', 'será', 'serán', 'serás', 'seré', 'seréis', 'sería', 'seríais', 'seríamos', 'serían',
+            'serías', 'si', 'sido', 'siendo', 'siente', 'sin', 'sintiendo', 'sobre', 'sois', 'somos', 'son', 'soy',
+            'su', 'sus', 'suya', 'suyas', 'suyo', 'suyos', 'sí', 'también', 'tanto', 'te', 'tendremos', 'tendrá',
+            'tendrán', 'tendrás', 'tendré', 'tendréis', 'tendría', 'tendríais', 'tendríamos', 'tendrían', 'tendrías',
+            'tened', 'tenemos', 'tener', 'tenga', 'tengamos', 'tengan', 'tengas', 'tengo', 'tengáis', 'tenida', 'tenidas',
+            'tenido', 'tenidos', 'teniendo', 'tenéis', 'tenía', 'teníais', 'teníamos', 'tenían', 'tenías', 'ti', 'tiempo',
+            'tiene', 'tienen', 'tienes', 'todo', 'todos', 'tu', 'tus', 'tuve', 'tuviera', 'tuvierais', 'tuvieran',
+            'tuvieras', 'tuvieron', 'tuviese', 'tuvieseis', 'tuviesen', 'tuvieses', 'tuvimos', 'tuviste', 'tuvisteis',
+            'tuviéramos', 'tuviésemos', 'tuvo', 'tuya', 'tuyas', 'tuyo', 'tuyos', 'tú', 'un', 'una', 'uno', 'unos',
+            'vosostras', 'vosostros', 'vuestra', 'vuestras', 'vuestro', 'vuestros', 'y', 'ya', 'yo'
+        ]
+        wordcloud = WordCloud(stopwords=stopwords).generate(comentarios_adicionales)
         
         # Display the generated image
         plt.imshow(wordcloud, interpolation='bilinear')
